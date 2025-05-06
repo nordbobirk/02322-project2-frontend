2ec13dbca0c3764f3f1c1396eddd941127de8b13
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2025-05-06 14:54:41 +0200

    card util functions

--- src/main/java/yukon/model/Card.java
+++ src/main/java/yukon/model/Card.java
@@ -1,7 +1,5 @@
 package yukon.model;
 
-import java.lang.invoke.StringConcatFactory;
-
 public class Card {
 
     private final Suit suit;
@@ -14,6 +12,47 @@ public class Card {
         this.number = number;
         this.shown = shown;
         this.next = next;
+    }
+
+    public static char numberToChar(int number) {
+        return switch (number) {
+            case 1 -> 'A';
+            case 10 -> 'T';
+            case 11 -> 'J';
+            case 12 -> 'Q';
+            case 13 -> 'K';
+            default -> ("" + number).charAt(0);
+        };
+    }
+
+    public static int getColumnSize(Card head) {
+        if (head == null) {
+            return 0;
+        }
+
+        int size = 1;
+        Card current = head;
+        while (current.getNext() != null) {
+            size++;
+            current = current.getNext();
+        }
+        return size;
+    }
+
+    public static Card getNthCard(Card columnHead, int n) {
+        if (n <= 0) {
+            return columnHead;
+        }
+
+        int index = 0;
+        Card current = columnHead;
+        while (current.getNext() != null) {
+            current = current.getNext();
+            index++;
+            if (index == n) return current;
+        }
+
+        return null;
     }
 
     public Card getTail() {
@@ -51,17 +90,6 @@ public class Card {
     @Override
     public String toString() {
         return String.format("%c%c%d", Card.numberToChar(number), Suit.toChar(suit), shown ? 1 : 0);
-    }
-
-    public static char numberToChar(int number) {
-        return switch (number) {
-            case 1 -> 'A';
-            case 10 -> 'T';
-            case 11 -> 'J';
-            case 12 -> 'Q';
-            case 13 -> 'K';
-            default -> ("" + number).charAt(0);
-        };
     }
 
 }
